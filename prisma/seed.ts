f86145bafcd5868dcf5676f30708d3caa5aa9880
Med <109179974+MedLines@users.noreply.github.com>
2025-11-22 00:19:58 +0200

    fixed some type issues

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,16 +7,22 @@ const tickets = [
     title: 'Ticket 1',
     content: 'This is the first ticket from the database.',
     status: 'DONE' as const,
+    deadline: new Date().toISOString(),
+    bounty: 0,
   },
   {
     title: 'Ticket 2',
     content: 'This is the second ticket from the database.',
     status: 'OPEN' as const,
+    deadline: new Date().toISOString(),
+    bounty: 0,
   },
   {
     title: 'Ticket 3',
     content: 'This is the third ticket from the database.',
     status: 'IN_PROGRESS' as const,
+    deadline: new Date().toISOString(),
+    bounty: 0,
   },
 ]
 
