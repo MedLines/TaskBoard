0ec6a69a9e26625f9441f75fcdee6325295c463b
Med <109179974+MedLines@users.noreply.github.com>
2025-11-23 14:37:58 +0200

    'lint fix'

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -4,16 +4,20 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { setCookieByKey } from '@/actions/cookies'
+import { fromErrorToActionState } from '@/components/form/utils/to-action-state'
 import { prisma } from '@/lib/prisma'
 import { ticketsPath } from '@/paths'
 
 export const deleteTicket = async (id: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-  await prisma.ticket.delete({
-    where: { id: id },
-  })
+  try {
+    await prisma.ticket.delete({
+      where: { id: id },
+    })
+  } catch (error) {
+    return fromErrorToActionState(error)
+  }
 
   revalidatePath(ticketsPath())
-  await setCookieByKey('toast', 'Ticket Deleted')
+  await setCookieByKey('toast', 'Ticket Del eted')
   redirect(ticketsPath())
 }
