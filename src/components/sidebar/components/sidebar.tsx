8cdf5d012b97e4291881014b4daf0dbc0d23af8f
Med <109179974+MedLines@users.noreply.github.com>
2025-12-01 16:09:40 +0200

    side bar css

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -19,13 +19,13 @@ const Sidebar = () => {
     setTimeout(() => setTransition(false), 200)
   }
   if (!user || isPending) {
-    return <div className="w-[78px] " /> //this so when the sidebar animates for the first time we don't get a layout shift
+    return <div className="w-[78px] bg-secondary/20 hidden" /> //this so when the sidebar animates for the first time we don't get a layout shift
   }
   return (
     <nav
       className={cn(
         'animate-sidebar-from-left',
-        'h-screen border-r pt-24',
+        'h-screen border-r pt-24 ',
         isTransition && 'duration-200',
         isOpen ? 'md:w-60 w-[78px]' : 'w-[78px]'
       )}
