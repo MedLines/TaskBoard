1c0fe7255f5345e3f6a2143a802d261e2363e1c1
Med <109179974+MedLines@users.noreply.github.com>
2025-12-07 21:19:30 +0200

    implement pagination for ticket list and add pagination component

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -7,18 +7,21 @@ export const getTickets = async (
   userId: string | undefined,
   searchParams: ParsedSearchParams
 ) => {
-  return await prisma.ticket.findMany({
-    where: {
-      userId,
-      //this line here because we added 'string[] | undefined' in type SearchParams
-      title: {
-        contains: searchParams.search || '',
-        mode: 'insensitive',
-      },
+  const where = {
+    userId,
+    title: {
+      contains: searchParams.search || '',
+      mode: 'insensitive' as const,
     },
+  }
+  const skip = searchParams.page * searchParams.size
+  const take = searchParams.size
+
+  return await prisma.ticket.findMany({
+    where,
+    skip,
+    take,
     orderBy: {
-      // ...(searchParams.sort === 'newest' && { createdAt: 'desc' }),
-      // ...(searchParams.sort === 'bounty' && { bounty: 'desc' }),
       [searchParams.sortKey]: searchParams.sortValue,
     },
     include: {
