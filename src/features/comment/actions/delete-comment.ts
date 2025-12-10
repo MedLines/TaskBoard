f6afe58b786f51e28a22d93eb31c6d94ab6ff7c9
Med <109179974+MedLines@users.noreply.github.com>
2025-12-10 15:21:20 +0200

    add delete comment functionality with confirmation dialog and button

---
+++ src/features/comment/actions/delete-comment.ts
@@ -0,0 +1,38 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+
+import {
+  fromErrorToActionState,
+  toActionState,
+} from '@/components/form/utils/to-action-state'
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect'
+import { isOwner } from '@/features/auth/utils/is-owner'
+import { prisma } from '@/lib/prisma'
+import { ticketPath } from '@/paths'
+
+const deleteComment = async (id: string) => {
+  const { user } = await getAuthOrRedirect()
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: id,
+    },
+  })
+  if (!comment || !isOwner(user, comment)) {
+    return toActionState('ERROR', 'Not Authorized')
+  }
+
+  try {
+    await prisma.comment.delete({
+      where: {
+        id: id,
+      },
+    })
+  } catch (error) {
+    return fromErrorToActionState(error)
+  }
+
+  revalidatePath(ticketPath(comment.id))
+  return toActionState('SUCCESS', 'Comment deleted!')
+}
+
+export { deleteComment }
--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,7 +1,10 @@
 import { Cardcompact } from '@/components/card-compact'
+import { getAuth } from '@/features/auth/queries/get-auth'
+import { isOwner } from '@/features/auth/utils/is-owner'
 
 import { getComments } from '../queries/get-comments'
 import { CommentCreateForm } from './comment-create-form'
+import { CommentDeleteButton } from './comment-delete-button'
 import { CommentItem } from './comment-item'
 
 type CommentsProps = {
@@ -10,6 +13,7 @@ type CommentsProps = {
 
 const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId)
+  const { user } = await getAuth()
   return (
     <>
       <Cardcompact
@@ -19,7 +23,15 @@ const Comments = async ({ ticketId }: CommentsProps) => {
       />
       <div className="flex flex-col gap-y-2 ml-8">
         {comments.map((comment) => (
-          <CommentItem key={comment.id} comment={comment} />
+          <CommentItem
+            key={comment.id}
+            comment={comment}
+            buttons={[
+              ...(isOwner(user, comment)
+                ? [<CommentDeleteButton key="0" id={comment.id} />]
+                : []),
+            ]} //key="0" just in case we want to add more buttons
+          />
         ))}
       </div>
     </>
